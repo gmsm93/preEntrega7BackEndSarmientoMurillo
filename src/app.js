01219ef3864cb3c8a11fd1ccc66553498d77411a
miguel <miguelsarmiento93@gmail.com>
2023-11-16 22:47:22 -0500

    Carga Inicial

--- src/app.js
+++ src/app.js
@@ -6,21 +6,22 @@ import __dirname from "./utils.js"
 import session from "express-session"
 import MongoStore from "connect-mongo"
 import bodyParser from "body-parser"
-
+import viewsRouter from './router/views.router.js'
+import sessionRouter from './router/session.router.js'
 
 // Inicializamos variables
 const app=express()
 const mongoURL = "mongodb+srv://r2:L53I9bfZi00L9BkV@clusterr2.028npj6.mongodb.net/?retryWrites=true&w=majority"
 const mongoDBName = 'ClusterR2'
 
-// traer info post como JSON
-app.use(express.json)
-app.use(express.urlencoded({extended:true}))
-
 // configurar el motor de plantillas
 app.engine('handlebars', handlebars.engine())
 app.set('views',__dirname + '/views' )
 app.set('view engine','handlebars')
+
+// traer info post como JSON
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
 
 // carpeta publica
 app.use(express.static(__dirname + '/public'))
@@ -28,6 +29,22 @@ app.use(express.static(__dirname + '/public'))
 // configuracion de rutas
 app.get('/health', (req, res) => res.send('ok'))
 app.use('/product', prdRouter)
+
+// Configuracion de la sesion
+app.use(session({
+  store: MongoStore.create({
+      mongoUrl,
+      dbName: mongoDB,
+      ttl: 100
+  }),
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use('/', viewsRouter)
+app.use('/api/session', sessionRouter)
+app.get('/health', (req, res) => res.send('OK'))
 
 // Conectar Mongo DB
 mongoose.connect(mongoURL, { dbName: mongoDBName })
