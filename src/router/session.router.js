bf2fd31ce737779b229ae451a1dec9110659b8e7
miguel <miguelsarmiento93@gmail.com>
2023-11-21 22:25:31 -0500

    Correcciones

--- src/router/session.router.js
+++ src/router/session.router.js
@@ -1,62 +1,86 @@
-import { Router } from "express"
-import UserModel from '../models/user.model.js'
-import bcrypt from 'bcrypt'
- 
+import { Router } from "express";
+import UserModel from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { createHash } from "../utils.js";
 
-const router = Router()
+const router = Router();
 
-router.post('/singup', async (req, res) => {
-    const { email, password } = req.body;
-    
-    try {
-      const existingUser = await UserModel.findOne({ email });
-  
-      if (existingUser) {
-        return res.status(400).send('El usuario ya existe');
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await UserModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'El usuario ya existe' });
+    }
+
+    const newUser = new UserModel({
+      email,
+      password: createHash(password),
+      role: email === 'adminCoder@coder.com' ? 'admin' : 'usuario'
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: 'Usuario creado correctamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error en el registro' });
+  }
+});
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local-login', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Error en el inicio de sesión' });
+    }
+
+    if (!user) {
+      return res.status(401).json({ message: 'Usuario no encontrado' });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Error en el inicio de sesión' });
       }
-  
-      const newUser = new User({
-        email,
-        password,
-        role: email === 'adminCoder@coder.com' ? 'admin' : 'user'
-      });
-  
-      await newUser.save();
-  
-      res.status(201).send('Usuario creado correctamente');
-    } catch (error) {
-      res.status(500).send('Error en el registro');
+
+      console.log(req.session.user);
+      res.redirect('/profile');
+    });
+  })(req, res, next);
+});
+
+router.get('/logout', async (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Error al cerrar sesión' });
+    } else {
+      res.redirect('/login');
     }
-})
+  });
+});
 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await UserModel.findOne({ email });
-        if (!user) {
-        return res.status(401).send('Credenciales incorrectas');
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-        return res.status(401).send('Credenciales incorrectas');
-        }
-        req.session.user = user;
-        res.redirect('/');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error en el login');
-    }
-})
+router.get(
+  '/login-github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  async (req, res) => {}
+);
 
-router.get('/logout', async(req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Error al cerrar sesión');
-        } else {
-          res.redirect('/login');
-        }
-      })
-})
+router.get(
+  '/githubcallback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  async (req, res) => {
+    console.log('Callback: ', req.user);
+    req.session.user = req.user;
 
-export default router
+    console.log(req.session);
+    res.redirect('/');
+  }
+);
+
+export default router;
--- src/router/views.router.js
+++ src/router/views.router.js
@@ -1,13 +1,10 @@
 import { Router } from 'express'
+import passport from 'passport'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-    if(req.session?.user) {
-        return res.redirect('/profile')
-    }
-    
-    return res.render('index', {})
+    res.render('index', {})
 })
 
 router.get('/login', (req, res) => {
@@ -17,23 +14,70 @@ router.get('/login', (req, res) => {
     res.render('login', {})
 })
 
-router.get('/singup', (req, res) => {
+router.get('/signup', (req, res) => {
     if(req.session?.user) {
         return res.redirect('/profile')
     }
 
-    res.render('singup', {})
+    res.render('signup', {})
 })
 
-router.get('/profile', auth, (req, res) => {
-    const user = req.session.user
+function authAdmin(req, res, next) {
+    if (req.isAuthenticated() && req.user) {
 
-    res.render('profile', user)
+        if (req.user.role === 'admin') {
+            return next();  
+        } else {
+            res.status(403).send('No tienes permisos de administrador');
+        }
+    } else {
+        res.redirect('/login');
+    }
+}
+
+router.get('/profile', authAdmin, (req, res) => {
+    if (req.isAuthenticated() && req.user) {
+        const user = req.user;
+        res.render('profile', user);
+    } else {
+        res.redirect('/login');
+    }
+});
+
+router.get('/logins', (req, res) => {
+    res.render('logins', {})
 })
+
+router.get('/login-github',
+    passport.authenticate('github', {scope: ['user:email']}),
+    async (req, res) => {}
+)
+
+router.get('/githubcallback',
+    passport.authenticate('github', {failureRedirect: '/'}),
+    async(req, res) => {
+        console.log('Callback: ', req.user)
+        req.session.user = req.user
+
+        console.log(req.session)
+        res.redirect('/')
+    }
+)
 
 function auth(req, res, next) {
-    if(req.session?.user) return next()
-    res.redirect('/')
+    if(req.session?.user) next()
+
+    return res.status(401).send('Auth error')
 }
+
+// function authAmdmin(req, res, next) {
+//     if(req.session?.user && req.session.user.role === 'admin') next()
+
+//     return res.status(401).send('Auth error')
+// }
+
+router.get('/private', auth, (req, res) => {
+    res.json(req.session.user)
+})
 
 export default router
